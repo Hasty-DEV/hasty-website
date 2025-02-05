495f3dfd225338eb72de250c47c635fadda52f18
HugoZAuad <hugozeymer@hotmail.com>
2025-02-05 12:01:43 -0300

    Adicionado primeiras informações para nova aba de serviços

--- src/view/screens/partials/Services.tsx
+++ src/view/screens/partials/Services.tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import { TbWorldCode } from "react-icons/tb";
 import { IoRocket } from "react-icons/io5";
 import { CiSettings } from "react-icons/ci";
@@ -22,62 +22,103 @@ const Services = () => {
       id="services"
     >
       <span className="text-xl text-indigo-500 font-bold">
-        {t('services.subtitle')}
+        {t("services.subtitle")}
       </span>
-      <h2 className="text-3xl mb-6 font-bold">
-        {t('services.title')}
-      </h2>
+      <h2 className="text-3xl mb-6 font-bold">{t("services.title")}</h2>
       <div className="w-full flex flex-wrap justify-center">
         <Card
           icon={<TbWorldCode />}
-          title={t('services.cards.0.title')}
-          description={t('services.cards.0.description')}
+          title={t("services.cards.0.title")}
+          description={t("services.cards.0.description")}
         />
         <Card
           icon={<IoRocket />}
-          title={t('services.cards.1.title')}
-          description={t('services.cards.1.description')}
+          title={t("services.cards.1.title")}
+          description={t("services.cards.1.description")}
         />
         <Card
           icon={<CiSettings />}
-          title={t('services.cards.2.title')}
-          description={t('services.cards.2.description')}
+          title={t("services.cards.2.title")}
+          description={t("services.cards.2.description")}
         />
         <Card
           icon={<MdSupportAgent />}
-          title={t('services.cards.3.title')}
-          description={t('services.cards.3.description')}
+          title={t("services.cards.3.title")}
+          description={t("services.cards.3.description")}
         />
         <Card
           icon={<FaSearch />}
-          title={t('services.cards.4.title')}
-          description={t('services.cards.4.description')}
+          title={t("services.cards.4.title")}
+          description={t("services.cards.4.description")}
         />
         <Card
           icon={<FaCog />}
-          title={t('services.cards.5.title')}
-          description={t('services.cards.5.description')}
+          title={t("services.cards.5.title")}
+          description={t("services.cards.5.description")}
         />
         <Card
           icon={<FaCode />}
-          title={t('services.cards.6.title')}
-          description={t('services.cards.6.description')}
+          title={t("services.cards.6.title")}
+          description={t("services.cards.6.description")}
         />
         <Card
           icon={<FaDesktop />}
-          title={t('services.cards.7.title')}
-          description={t('services.cards.7.description')}
+          title={t("services.cards.7.title")}
+          description={t("services.cards.7.description")}
         />
         <Card
           icon={<FaMobileAlt />}
-          title={t('services.cards.8.title')}
-          description={t('services.cards.8.description')}
+          title={t("services.cards.8.title")}
+          description={t("services.cards.8.description")}
         />
         <Card
           icon={<FaTools />}
-          title={t('services.cards.9.title')}
-          description={t('services.cards.9.description')}
+          title={t("services.cards.9.title")}
+          description={t("services.cards.9.description")}
         />
+      </div>
+
+      <div className="mb-6 pb-4 shadow-bottom">
+        <h1 className="font-bold text-4xl text-left">
+          DESENVOLVIMENTO DE SITE E APLICATIVOS
+        </h1>
+        <h4 className="text-left text-sm">
+          Criamos sites e aplicativos do zero. Implementação, ajustes e
+          manutenções.
+        </h4>
+        <p className="text-left">Leia mais (seta diagonal)</p>
+      </div>
+
+      <div className="mb-6 pb-4 shadow-bottom">
+        <h1 className="font-bold text-4xl text-left">
+          DESENVOLVIMENTO PERSONALIZADO
+        </h1>
+        <h4 className="text-left text-sm">
+          Somos especialistas em desenvolvimento personalizado para startups.
+        </h4>
+        <p className="text-left">Leia mais (seta diagonal)</p>
+      </div>
+
+      <div className="mb-6 pb-4 shadow-bottom">
+        <h1 className="font-bold text-4xl text-left">
+          IDENTIDADE VISUAL COMPLETA
+        </h1>
+        <h4 className="text-left text-sm">
+          Seja marcante memorável e cative a atenção do público.
+        </h4>
+        <p className="text-left">Leia mais (seta diagonal)</p>
+      </div>
+
+      <div className="mb-6 pb-4 shadow-bottom">
+        <h1 className="font-bold text-4xl text-left">
+          CRIAMOS PRODUTOS DIGITAIS E SUA ESTRATÉGIAS DE MARKETING
+        </h1>
+        <h4 className="text-left whitespace-pre-line text-sm">
+          Com nosso processo de desenvolvimento ágil, criamos sites, landing
+          pages, blogs, hotsites e portais gerenciáveis com tempo e custo
+          reduzidos e que vão fazer suas estratégias de marketing decolar!
+        </h4>
+        <p className="text-left">Leia mais (seta diagonal)</p>
       </div>
     </section>
   );
